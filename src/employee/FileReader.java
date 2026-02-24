82795876e539b3575de478e7a05023165c9ab3c6
RisingSatalite <saihajmann07@gmail.com>
2026-02-24 12:05:26 -0700

    Update FileReader.java

--- src/employee/FileReader.java
+++ src/employee/FileReader.java
@@ -2,23 +2,48 @@ package employee;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
+
 import employee.Employee;
 
 public class FileReader {
-	public static void main(String[] args) {
-	    File myObj = new File("employeeList/employeesWithoutRepeat.txt");
-	    
+	public ArrayList EmployeeList = new ArrayList<>();
+	
+	public ArrayList returnArray() {
+		File myObj = new File("employeeList/employeesWithoutRepeat.txt");    
 	    
 	    try (Scanner myReader = new Scanner(myObj)) {
 	        while (myReader.hasNextLine()) {
 	          String data = myReader.nextLine();
 	          System.out.println(data);
+	          EmployeeList.add();
 	        }
 	      } catch (FileNotFoundException e) {
 	        System.out.println("An error occurred.");
 	        e.printStackTrace();
 	      }
+	}
+	
+	public ArrayList returnArray(String file) {
+		File myObj = new File(file);    
+	    
+	    try (Scanner myReader = new Scanner(myObj)) {
+	        while (myReader.hasNextLine()) {
+	          String data = myReader.nextLine();
+	          System.out.println(data);
+	          EmployeeList.add();
+	        }
+	      } catch (FileNotFoundException e) {
+	        System.out.println("An error occurred.");
+	        e.printStackTrace();
+	      }
+	}
+	
+	public static void main(String[] args) {
+		FileReader reader = new FileReader();
+		reader.returnArray();
+	    
 	}
 
 }
